89a7831cbe3e300cc303f45766871b73d35e7967
Yigang Wang <ico.yigang@hotmail.com>
2016-05-09 14:01:49 -0400

    Create client.go

---
+++ src/pbservice/client.go
@@ -0,0 +1,145 @@
+package pbservice
+
+import "viewservice"
+import "net/rpc"
+import "fmt"
+import "time"
+import "crypto/rand"
+import "math/big"
+
+
+type Clerk struct {
+	vs *viewservice.Clerk
+	// Your declarations here
+	view viewservice.View
+	me string
+}
+
+// this may come in handy.
+func nrand() int64 {
+	max := big.NewInt(int64(1) << 62)
+	bigx, _ := rand.Int(rand.Reader, max)
+	x := bigx.Int64()
+	return x
+}
+
+func MakeClerk(vshost string, me string) *Clerk {
+	ck := new(Clerk)
+	ck.vs = viewservice.MakeClerk(me, vshost)
+	// Your ck.* initializations here
+	ck.view = viewservice.View{}
+	ck.me = strconv.FormatInt(nrand(), 10)
+
+	return ck
+}
+
+
+//
+// call() sends an RPC to the rpcname handler on server srv
+// with arguments args, waits for the reply, and leaves the
+// reply in reply. the reply argument should be a pointer
+// to a reply structure.
+//
+// the return value is true if the server responded, and false
+// if call() was not able to contact the server. in particular,
+// the reply's contents are only valid if call() returned true.
+//
+// you should assume that call() will return an
+// error after a while if the server is dead.
+// don't provide your own time-out mechanism.
+//
+// please use call() to send all RPCs, in client.go and server.go.
+// please don't change this function.
+//
+func call(srv string, rpcname string,
+	args interface{}, reply interface{}) bool {
+	c, errx := rpc.Dial("unix", srv)
+	if errx != nil {
+		return false
+	}
+	defer c.Close()
+
+	err := c.Call(rpcname, args, reply)
+	if err == nil {
+		return true
+	}
+
+	fmt.Println(err)
+	return false
+}
+
+func (ck *Clerk) UpdateView() {
+	view, err := ck.vs.Ping(ck.view.Viewnum)
+	if err != nil {
+		return
+	}
+	ck.view = view
+}
+
+//
+// fetch a key's value from the current primary;
+// if the key has never been set, return "".
+// Get() must keep trying until it either the
+// primary replies with the value or the primary
+// says the key doesn't exist (has never been Put().
+//
+func (ck *Clerk) Get(key string) string {
+
+	// Your code here.
+	if ck.view.View == 0 {
+		ck.UpdateView()
+	}
+	args := &GetArgs{Key, nrand()}
+	var reply GetReply
+
+	for {
+		ok := call(ck.view.Primary, "PBServer.Get", args, &reply)
+		if ok {
+			return reply.value
+		}
+		time.Sleep(viewservice.PingInterval)
+		ck.UpdateView()
+	}
+
+	return "???"
+}
+
+//
+// send a Put or Append RPC
+//
+func (ck *Clerk) PutAppend(key string, value string, op string) {
+
+	// Your code here.
+	if ck.view.Viewnum == 0 {
+		ck.UpdateView()
+	}
+	args := &PutAppendArgs{key, value, op, false, strconv.FormatInt(nrand(), 10), ck.me}
+	var reply PutReply
+
+	for {
+		ok := call(ck.view.Primary, "PBServer.Put", args, &reply)
+		if ok {
+			return reply.PreviousValue
+		}
+		time.Sleep(viewservice.PingInterval)
+		ck.UpdateView()
+	}
+	return "???"
+}
+
+
+//
+// tell the primary to update key's value.
+// must keep trying until it succeeds.
+//
+func (ck *Clerk) Put(key string, value string) {
+	ck.PutAppend(key, value, "Put")
+}
+
+//
+// tell the primary to append to key's value.
+// must keep trying until it succeeds.
+//
+func (ck *Clerk) Append(key string, value string) {
+	ck.PutAppend(key, value, "Append")
+}
